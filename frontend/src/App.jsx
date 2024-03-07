fb0296aa3d6d857f2ffa1af9cedd203100b98171
Matt Cannon <mttcannon@gmail.com>
2024-03-07 10:22:42 -0800

    frontend: ContactList and ContactForm added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+import ContactList from './ContactList'
+import ContactForm from './ContactForm'
+
+
 import './App.css'
 
-function App() {
 
-  return (
-    <>
+function App() {
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    fetchContacts()
+  }, [])
+
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts")
+    const data = await response.json()
+    setContacts(data.contacts)
+    console.log(data.contacts)
+  }
+
+  return <>
+      <ContactList contacts={contacts} />
+      <ContactForm />
     </>
-  )
 }
 
 export default App
